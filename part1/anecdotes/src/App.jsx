bbe0d4b104440c785213d0b4c0aad8900d5bdca7
ghshenavar <ghshenavar@gmail.com>
2024-07-20 19:24:21 +0300

    anecdotes step 2

--- part1/anecdotes/src/App.jsx
+++ part1/anecdotes/src/App.jsx
@@ -19,14 +19,23 @@ const App = () => {
   ]
    
   const [selected, setSelected] = useState(0)
+  const [votes, setVotes] = useState(Array(anecdotes.length).fill(0))
 
   const selectNew = () => {
     setSelected(Math.floor(Math.random() * anecdotes.length))
   }
 
+  const onVote = () => {
+    const newVotes = [...votes]
+    newVotes[selected] += 1
+    setVotes(newVotes)
+  }
+
   return (
     <div>
       <p>{anecdotes[selected]}</p>
+      <p>has {votes[selected]} votes</p>
+      <Button handleClick={onVote} text="vote" />
       <Button handleClick={selectNew} text="next anecdote" />
     </div>
   )
