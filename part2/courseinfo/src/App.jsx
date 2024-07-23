6c0d999648c1f2c6b0623e09b73fa1471087c0a5
ghshenavar <ghshenavar@gmail.com>
2024-07-23 11:42:52 +0300

    2.1 to 2.3 done

---
+++ part2/courseinfo/src/App.jsx
@@ -0,0 +1,61 @@
+import { useState } from 'react'
+
+const Header = ({ course }) => <h1>{course}</h1>
+
+const Total = ({ sum }) => <b>total of {sum} exercises</b>
+
+const Part = ({ part }) => 
+  <p>
+    {part.name} {part.exercises}
+  </p>
+
+const Content = ({ parts }) => (
+  <>
+        {parts.map(part =>           
+        <Part key={part.id} 
+        part={part} />        
+      )}      
+  </>
+)
+
+
+const Course = ({course}) => (
+  <div>
+      <Header course={course.name} />
+      <Content parts={course.parts} />
+      <Total sum={course.parts.reduce((s, p) => s + p.exercises, 0,)} />
+    </div>
+)
+
+const App = () => {
+  const course = {
+    id: 1,
+    name: 'Half Stack application development',
+    parts: [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10,
+        id: 1
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7,
+        id: 2
+      },
+      {
+        name: 'State of a component',
+        exercises: 14,
+        id: 3
+      },
+      {
+        name: 'overload data',
+        exercises: 23,
+        id: 45
+      },
+    ]
+  }
+
+  return <Course course={course} />
+}
+
+export default App
