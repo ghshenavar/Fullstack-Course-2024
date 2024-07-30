e12fb50017ac8a055dae8496c852b68ebb2e8078
ghshenavar <ghshenavar@gmail.com>
2024-07-30 12:10:46 +0300

    linting dióne, end of part 3

--- part3/phonebook/mongo.js
+++ part3/phonebook/mongo.js
@@ -16,38 +16,40 @@ mongoose.set('strictQuery',false)
 mongoose.connect(url)
 
 const personSchema = new mongoose.Schema({
-    name: String,
-    number: String,
-  })
+  name: String,
+  number: String,
+})
 
 const Person = mongoose.model('Person', personSchema)
 
 if(process.argv.length===3){
-  
-    Person.find({}).then(result => {
-      console.log('phonebook: ')
-      result.forEach(person => {
-        console.log(`${person.name} ${person.number}`)
-      })
+
+  Person.find({}).then(result => {
+    console.log('phonebook: ')
+    result.forEach(person => {
+      console.log(`${person.name} ${person.number}`)
+    })
+    mongoose.connection.close()
+  })
+    .catch((err) => {
+      console.log('Error!', err)
       mongoose.connection.close()
     })
-    .catch((err) => {
-        console.log("Error!", err)
-        mongoose.connection.close()
-      });
-  
-  }else{
-    const person = new Person({
-      name: process.argv[3],
-      number: process.argv[4],
-    }) 
-    
-    person.save().then(result => {
+
+}else{
+  const person = new Person({
+    name: process.argv[3],
+    number: process.argv[4],
+  })
+
+  person
+    .save()
+    .then(result => {
       console.log(`added ${result.name} number ${result.number} to phonebook`)
       mongoose.connection.close()
     })
     .catch((err) => {
-        console.log("Error!", err)
-        mongoose.connection.close()
-      });
-  }
+      console.log('Error!', err)
+      mongoose.connection.close()
+    })
+}
